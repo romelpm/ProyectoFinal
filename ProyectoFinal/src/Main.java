aef3575bf71ea7bc6cd28fc23dc20afe89eaceab
yefers <yefers@yefers_yoniel>
2015-06-13 12:26:39 -0500

    se aumento otra clase main

---
+++ ProyectoFinal/src/Main.java
@@ -0,0 +1,28 @@
+
+//import javax.swing.JOptionPane;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author yefers
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       // JOptionPane.showMessageDialog(null, "Hola mama :D");
+        int a = 3;
+        int b = 4;
+        System.out.println("el nuemro es : " + a*b);
+        
+    }
+    
+}
